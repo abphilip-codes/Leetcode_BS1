7a0962b1307fb2efefe6ee3b20ef5eabeae3b525
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-24 19:52:18 +0530

    Peak Index in a Mountain Array

--- 2/2.py
+++ 2/2.py
@@ -1,6 +1,11 @@
-# 1523
-# https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+# 852
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/
 
 class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        return (high - low + (low % 2) + (high % 2)) // 2
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr)-1
+        while(l<r):
+            mid = l+(r-l)//2
+            if(arr[mid]>=arr[mid+1]): r = mid
+            elif(arr[mid]<arr[mid+1]): l = mid + 1
+        return l
