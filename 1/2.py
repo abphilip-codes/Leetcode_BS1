daecee432481dd530f9ae725b94bae8d9e09d938
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-24 19:41:07 +0530

    Guess Number Higher or Lower

--- 1/2.py
+++ 1/2.py
@@ -1,6 +1,19 @@
 # 374
 # https://leetcode.com/problems/guess-number-higher-or-lower/
 
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
 class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        return (high - low + (low % 2) + (high % 2)) // 2
+    def guessNumber(self, n: int) -> int:
+        l, r = 1, n
+        while(l<r):
+            mid = (l+r)//2
+            if(guess(mid)==0): return mid
+            elif(guess(mid)==-1): r = mid
+            elif(guess(mid)==1): l = mid+1                
+        return l
