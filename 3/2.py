5efd8c32d771da19407e3b52c5488d87690d05a6
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-25 14:02:24 +0530

    Not Completed

--- 3/2.py
+++ 3/2.py
@@ -1,11 +1,18 @@
-# 852
-# https://leetcode.com/problems/peak-index-in-a-mountain-array/
+# 1385
+# https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
 
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        l, r = 0, len(arr)-1
-        while(l<r):
-            mid = l+(r-l)//2
-            if(arr[mid]>=arr[mid+1]): r = mid
-            elif(arr[mid]<arr[mid+1]): l = mid + 1
-        return l
+        def bs(l,r,arr,k1,k2):
+            while l<=r:
+                mid=(l+r)//2 
+                if(k1<=arr[mid]<=k2): return True
+                elif arr[mid]>k2: r=mid-1
+                else: l=mid+1 
+            return False 
+        
+        n=len(arr2)
+        c=0
+        for ele in arr1:
+            if not bs(0,n-1,arr2,ele-d,ele+d): c+=1
+        return c
