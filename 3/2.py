501a277d88dfe248922029f0d4b5c989372446a1
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-26 11:37:31 +0530

    Find the Distance Value Between Two Arrays

--- 3/2.py
+++ 3/2.py
@@ -2,17 +2,16 @@
 # https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
 
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        arr2.sort()
         def bs(l,r,arr,k1,k2):
-            while l<=r:
-                mid=(l+r)//2 
+            while(l<=r):
+                mid = (l+r)//2 
                 if(k1<=arr[mid]<=k2): return True
-                elif arr[mid]>k2: r=mid-1
-                else: l=mid+1 
+                elif(arr[mid]>k2): r = mid-1
+                elif(arr[mid]<k1): l = mid+1 
             return False 
-        
-        n=len(arr2)
-        c=0
-        for ele in arr1:
-            if not bs(0,n-1,arr2,ele-d,ele+d): c+=1
+        n, c = len(arr2), 0
+        for z in arr1:
+            if not bs(0,n-1,arr2,z-d,z+d): c+=1
         return c
