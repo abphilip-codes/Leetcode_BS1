39e00d9c26499a075a82616e830d9c9bf9665496
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-26 11:19:26 +0530

    Sqrt(x)

--- 4/1.py
+++ 4/1.py
@@ -4,9 +4,9 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         if(x<2): return x
-        l, r = x, x//2
+        l, r = 2, x//2
         while(l<=r):
-            mid = l+(r-l)//2;
+            mid = l+(r-l)//2
             s = mid**2
             if(s==x): return mid
             elif(s<x): l = mid+1
