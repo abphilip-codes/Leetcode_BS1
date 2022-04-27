f332250e623a18ca61b196bcccc32e17f33dffcb
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-27 14:29:44 +0530

    Arranging Coins

--- 6/1.py
+++ 6/1.py
@@ -3,8 +3,10 @@
 
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        ans = 0
-        while(n>ans):
-            ans+=1
-            n-=ans
-        return ans
+        if(n==1): return 1
+        l, r = 1, n
+        while(l<r):
+            m = (l+r)//2
+            if((m*(m+1))//2>n): r = m
+            else: l = m+1
+        return l-1
