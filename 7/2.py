b2d1f02419c1638517db7b3975258150b35b7877
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-27 14:42:26 +0530

    Special Array With X Elements Greater Than or Equal X

--- 7/2.py
+++ 7/2.py
@@ -2,14 +2,15 @@
 # https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
 
 class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        # for z in letters:
-        #     if(ord(z)>ord(target)): return z
-        # else: return letters[0]
-        l, r = 0, len(letters)-1
-        ans = letters[0]
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        if(n<=nums[0]): return n
+        l, r = 0, n
         while(l<=r):
-            mid = l+(r-l)//2
-            if(ord(letters[mid])<=ord(target)): l = mid+1
-            else: ans, r = letters[mid], mid-1
-        return ans
+            mid = (l+r)//2
+            ans = sum([1 if(nums[z]>=mid) else 0 for z in range(n)])
+            if(ans==mid): return ans
+            elif(ans<mid): r = mid-1
+            elif(ans>mid): l = mid+1
+        return -1
