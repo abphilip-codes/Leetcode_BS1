5d81c0a89e414ad75141528842353537eaba79c2
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 08:16:59 +0530

    The K Weakest Rows in a Matrix

--- 9/1.py
+++ 9/1.py
@@ -2,10 +2,10 @@
 # https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers)-1
-        while(l<r):
-            if(numbers[l]+numbers[r]==target): return [l+1, r+1]
-            elif(numbers[l]+numbers[r]<target): l+=1
-            elif(numbers[l]+numbers[r]>target): r-=1
-        return [-1,-1]
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        ans, h = [], []
+        for z in range(len(mat)): heappush(h, (sum(mat[z]), z))
+        for z in range(k): 
+            a, b = heappop(h)
+            ans.append(b)
+        return ans
