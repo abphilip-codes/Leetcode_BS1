320691abb57de525b5bf4311e5544086f06c14a8
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 09:47:13 +0530

    Intersection of Two Arrays II

--- X/1.py
+++ X/1.py
@@ -2,10 +2,5 @@
 # https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers)-1
-        while(l<r):
-            if(numbers[l]+numbers[r]==target): return [l+1, r+1]
-            elif(numbers[l]+numbers[r]<target): l+=1
-            elif(numbers[l]+numbers[r]>target): r-=1
-        return [-1,-1]
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list((Counter(nums1)&Counter(nums2)).elements())
