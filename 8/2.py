f54fc85585e41ab25e47adfeb00d4b62eaa3e5dd
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 08:11:45 +0530

    Search a 2D Matrix

--- 8/2.py
+++ 8/2.py
@@ -2,15 +2,6 @@
 # https://leetcode.com/problems/search-a-2d-matrix/
 
 class Solution:
-    def specialArray(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        if(n<=nums[0]): return n
-        l, r = 0, n
-        while(l<=r):
-            mid = (l+r)//2
-            ans = sum([1 if(nums[z]>=mid) else 0 for z in range(n)])
-            if(ans==mid): return ans
-            elif(ans<mid): r = mid-1
-            elif(ans>mid): l = mid+1
-        return -1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        k = bisect_left(matrix, target, key = lambda z:z[-1])
+        return (k<len(matrix) and matrix[k][bisect_left(matrix[k],target)]==target)
