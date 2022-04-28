a7724c0d427e628e3a56908555aaf65307ba49c3
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-28 09:44:58 +0530

    Check If N and Its Double Exist

--- 9/2.py
+++ 9/2.py
@@ -2,15 +2,9 @@
 # https://leetcode.com/problems/check-if-n-and-its-double-exist/
 
 class Solution:
-    def specialArray(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        if(n<=nums[0]): return n
-        l, r = 0, n
-        while(l<=r):
-            mid = (l+r)//2
-            ans = sum([1 if(nums[z]>=mid) else 0 for z in range(n)])
-            if(ans==mid): return ans
-            elif(ans<mid): r = mid-1
-            elif(ans>mid): l = mid+1
-        return -1
+    def checkIfExist(self, arr: List[int]) -> bool:
+        k = 0   
+        for z in arr:
+            if(z*2 in arr and z): return True
+            elif not z: k+=1
+        return True if(k>1) else False
