928cc94eae33868289aeecaa4a3dd5751130e6e4
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-29 16:45:35 +0530

    Maximum Distance Between a Pair of Values

--- XI/1.py
+++ XI/1.py
@@ -4,12 +4,10 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
         ans = 0
-        for z in range(len(nums1)):
-            l, r = z, len(nums2)-1
-            while(l<=r):
-                mid = (l+r)//2
-                if(nums1[z]<=nums2[mid]):
-                    l = mid+1
-                    if((mid-z)>ans): ans = mid-z
-                elif(nums1[z]>nums2[mid]): r = mid-1
+        z, y = 0, 0
+        while(z<len(nums1) and y<len(nums2)):
+            if(nums1[z]>nums2[y]): z+=1
+            elif(nums1[z]<=nums2[y]):
+                ans = max(ans, y-z)
+                y+=1
         return ans
