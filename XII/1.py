288be40ea8f27bff7df8331bea93ef0075283a82
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-29 17:53:28 +0530

    Find Minimum in Rotated Sorted Array

--- XII/1.py
+++ XII/1.py
@@ -2,5 +2,11 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list((Counter(nums1)&Counter(nums2)).elements())
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        if(nums[0]<=nums[len(nums)-1]): return nums[0]
+        while(l<=r):
+            mid = (l+r)//2
+            if(nums[mid]<nums[mid-1]): return nums[mid]
+            elif(nums[mid]<nums[0]): r = mid-1
+            else: l = mid+1
