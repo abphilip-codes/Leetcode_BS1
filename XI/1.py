6ec3d7eb50b09df623980de2530ef4c44fc5bcfa
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-29 16:43:15 +0530

    Maximum Distance Between a Pair of Values

--- XI/1.py
+++ XI/1.py
@@ -2,5 +2,14 @@
 # https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/
 
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list((Counter(nums1)&Counter(nums2)).elements())
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+        for z in range(len(nums1)):
+            l, r = z, len(nums2)-1
+            while(l<=r):
+                mid = (l+r)//2
+                if(nums1[z]<=nums2[mid]):
+                    l = mid+1
+                    if((mid-z)>ans): ans = mid-z
+                elif(nums1[z]>nums2[mid]): r = mid-1
+        return ans
